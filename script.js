276898e6e26ef60a6a4078ecfeb06eb595740888
Aniket Meena <139353764+aniketmeena004@users.noreply.github.com>
2024-06-19 19:30:36 +0530

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let image = document.getElementById("qrimage");
 let inputtext = document.getElementById("qrtext");
 
 function qrfunction(){
-    image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ inputtext.value;
-    image.style.zIndex = 1;
+    if(inputtext.value === ""){
+        alert("Cannot Generate QR for empty Value")
+    }
+    else{
+        image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ inputtext.value;
+        image.style.zIndex = 1;
+    }
 }
+inputtext.addEventListener('keydown', function(event){
+    if(event.key == 'Enter'){
+        qrfunction();
+    }
+})
