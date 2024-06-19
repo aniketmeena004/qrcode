ec382035bc5f72c959959d48ab421f4b5a88a118
Aniket Meena <139353764+aniketmeena004@users.noreply.github.com>
2024-06-19 13:06:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let image = document.getElementById("qrimage");
+let inputtext = document.getElementById("qrtext");
+
+function qrfunction(){
+    image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ inputtext.value;
+}
