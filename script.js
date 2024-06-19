33a72f87dca6577e6f7416b535567dd1327f79d5
Aniket Meena <139353764+aniketmeena004@users.noreply.github.com>
2024-06-19 17:03:04 +0530

    script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ let inputtext = document.getElementById("qrtext");
 
 function qrfunction(){
     image.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ inputtext.value;
+    image.style.zIndex = 1;
 }
